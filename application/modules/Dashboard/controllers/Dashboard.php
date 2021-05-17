be771f945692564323eb0d9ca2d83be1079bb945
idrisabdul <idrisaziz52@gmail.com>
2021-05-17 14:32:34 +0700

    Add All Files

---
+++ application/modules/Dashboard/controllers/Dashboard.php
@@ -0,0 +1,14 @@
+<?php defined('BASEPATH') or exit('No direct script access allowed');
+
+class Dashboard extends MX_Controller
+{
+    function __construct()
+    {
+        parent::__construct();
+    }
+
+    function index()
+    {
+        $this->template->load('template', 'halaman_dashboard');
+    }
+}
